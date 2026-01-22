9e095c276f011dd5487713bf330a0e785710ed59
Jēkabs Balodis <signalingpigeon@duck.com>
2026-01-22 22:07:14 +0200

    Add tasks to process the webpage and insert songs into database

--- update_charts.py
+++ update_charts.py
@@ -1,18 +1,20 @@
-from modulefinder import packagePathMap
+import duckdb
 import httpx
-from prefect import task
+import polars as pl
+from bs4 import BeautifulSoup, ResultSet, Tag
+from prefect import flow, task
 from prefect.tasks import exponential_backoff
 from pydantic import EmailStr, HttpUrl
-from bs4 import BeautifulSoup, ResultSet, Tag
+
 from config import config
-from models import ChartEntry, Song
+from models import Song
 
 
 @task(
     retries=10,
     retry_delay_seconds=exponential_backoff(backoff_factor=3),
 )
-def read_webpage(url: HttpUrl, email: EmailStr) -> httpx.Response:
+def fetch_webpage(url: HttpUrl, email: EmailStr) -> httpx.Response:
     """Read naba.lv webpage and return httpx response"""
     headers = {
         'user-agent': f'python-httpx {str(email)}',
@@ -25,31 +27,93 @@ def read_webpage(url: HttpUrl, email: EmailStr) -> httpx.Response:
 
 
 @task
-def parse_webpage(res: httpx.Response) -> BeautifulSoup:
+def parse_html(res: httpx.Response) -> BeautifulSoup:
     """Parse the returned response HTML text"""
     return BeautifulSoup(res.text, 'lxml')
 
 
 @task
-def extract_charts(soup: BeautifulSoup) -> ResultSet[Tag]:
-    """Return the elements for the top10 and top25 charts"""
-    return soup.select('.songsList')
+def extract_song_elements(soup: BeautifulSoup) -> ResultSet[Tag]:
+    """Return the elements for the songs"""
+    return soup.css.select('.songName')
 
 
 @task
-def parse_charts(soup: ResultSet[Tag]) -> list[Song]:
-    """Parse the part of the html page that contains information about top10 chart"""
-    top10_soup = soup[0].select('.songLine')
+def parse_song_data(soup: ResultSet[Tag]) -> list[Song]:
+    """Parse the part of the html page that contains information
+    about songs - artist and song name"""
+    songs = soup
 
     song_list: list[Song] = []
-    top10: list[ChartEntry]
 
-    for entry in top10_soup:
-        if entry.select_one('.songName') and entry.select_one('.songPlace') is None:
-            continue
-        web_songname = entry.select_one('.songName').string
-        place = entry.select_one('.songPlace').string
-        artist, song_name = web_songname.split(' - ')[0], web_songname.split(' - ')[1]
-        song = Song(artist=artist, song_name=song_name, web_songname=web_songname)
-        chart_entry = ChartEntry()
-        song_list.append(song)
+    for song in songs:
+        web_songname: str = song.text
+        artist, song_name = web_songname.split(' - ', 1)
+        song_list.append(
+            Song(
+                artist=artist,
+                song_name=song_name,
+                web_songname=web_songname,
+            )
+        )
+
+    return song_list
+
+
+@task
+def load_existing_songs() -> pl.DataFrame:
+    """Query database for records already available"""
+    with duckdb.connect(config.DB_PATH) as conn:
+        df = conn.sql('select * from songs').pl()
+
+    df.drop('id')
+    return df
+
+
+@task
+def create_web_songs_df(song_list: list[Song]) -> pl.DataFrame:
+    """Insert the parsed song values into polars dataframe"""
+    web_songs = pl.DataFrame(song_list)
+    web_songs = web_songs.drop('id')
+    return web_songs
+
+
+@task
+def filter_new_songs(
+    existing_songs: pl.DataFrame, web_songs: pl.DataFrame
+) -> pl.DataFrame:
+    """Anti-join the web songs with existing songs to return only new songs"""
+    new_songs = web_songs.join(
+        other=existing_songs,
+        on=['web_songname'],
+        how='anti',
+    )
+    return new_songs
+
+
+@task
+def insert_songs_into_db(new_songs: pl.DataFrame) -> None:
+    """Write the dataframe of new songs into database"""
+    with duckdb.connect(config.DB_PATH) as conn:
+        conn.execute(
+            """-- sql
+            insert into songs by name (
+                select * from new_songs
+            )
+            """
+        )
+
+
+@flow
+def update_charts(url: HttpUrl, email: EmailStr) -> None:
+    """Main flow to fetch the webpage, parse html, extract song elements
+    and parse song data, load existing songs to filter only new songs
+    and finaly insert songs into database."""
+    res = fetch_webpage(url=config.FLOW_URL, email=config.FLOW_EMAIL)
+    soup = parse_html(res=res)
+    song_soup = extract_song_elements(soup=soup)
+    songs = parse_song_data(soup=song_soup)
+    existing_songs = load_existing_songs()
+    web_songs = create_web_songs_df(songs)
+    new_songs = filter_new_songs(existing_songs, web_songs)
+    insert_songs_into_db(new_songs)
