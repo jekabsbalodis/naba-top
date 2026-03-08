3160d11d1c7a7f7d8fcce3af5742028f0ec9f6b2
Jēkabs Balodis <signalingpigeon@duck.com>
2026-03-09 00:50:17 +0200

    Refactoring update s3 storage task

--- app/data/get_data.py
+++ app/data/get_data.py
@@ -9,6 +9,11 @@ from database.s3_connection import s3_connection
 ViewName = Literal['songs', 'charts', 'top10', 'top25', 'all_songs_ranked']
 ALLOWED_VIEWS: frozenset[str] = frozenset(get_args(ViewName))
 
+key_id = st.secrets['garage']['key_id']
+secret = st.secrets['garage']['secret']
+endpoint = st.secrets['garage']['endpoint']
+region = st.secrets['garage']['region']
+
 
 @st.cache_data(ttl=60 * 60 * 24, show_spinner='Lejuplādē datus...', show_time=True)
 def get_view(*, view: ViewName) -> pl.DataFrame:
@@ -24,7 +29,12 @@ def get_view(*, view: ViewName) -> pl.DataFrame:
             f"""Norādīts neatļauts skata vai tabulas nosaukums,
             izvēlies kādu no {ALLOWED_VIEWS}."""
         )
-    with s3_connection() as conn:
+    with s3_connection(
+        key_id=key_id,
+        secret=secret,
+        endpoint=endpoint,
+        region=region,
+    ) as conn:
         df = conn.sql(
             f"""--sql
             select
